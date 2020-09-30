23636f8eb7ad7d2ed88c9abb05b4179b0bdd39b9
tyler <jboydmeredith@gmail.com>
2020-09-30 16:33:44 -0400

    speed up get_spikeinfo

--- get_spikeinfo.m
+++ get_spikeinfo.m
@@ -1,40 +1,16 @@
 function spikeinfo = get_spikeinfo(ratname)
 
-% get session ids for this rat and date range
-sessid   = bdata('select sessid from sessions where ratname="{S}"',ratname);
-    
-% remove sessions without phys
-which_phys = false(size(sessid));
-for ss = 1:length(sessid)
-    this_sessid = sessid(ss);
-    is_phys_sess = ~isempty(bdata('select sessid from phys_sess where ratname="{S}" and sessid={S}',ratname,num2str(this_sessid)));
-    if is_phys_sess
-        which_phys(ss) = true;
-    end
-end
-phys_sessid = sessid(which_phys);
-%%
+% get list of sessions and cells for this rat
+[sessid, cellid] = bdata('select sessid, cellid from cells where ratname="{S}"',ratname);
 
-% select the cellids from the phys sessions
-cellid      = [];
-is_single   = [];
-channels    = [];
-sessid      = [];
-for i=1:length(phys_sessid)
-[t_cellid, t_is_single, t_channels,t_sessid] = bdata(['select cellid, single, sc_num,sessid from cells where sessid="{S}"'], num2str(phys_sessid(i)));
-cellid = [cellid; t_cellid];
-is_single = [is_single; t_is_single];
-channels = [channels; t_channels];
-sessid = [sessid; t_sessid];
-end
-
-%%
 % get the spike times for these cells
-spktime = cell(1,length(cellid));
+is_single   = false(length(cellid),1);
+channels    = zeros(length(cellid),1);
+spktime     = cell(1,length(cellid));
 for cc = 1:length(cellid)
-    this_ts = bdata('select ts from spktimes where cellid={S}',num2str(cellid(cc)));
+    this_ts = bdata('select ts from spktimes where cellid={S}',cellid(cc));
     spktime{cc} = this_ts{:};
-    
+    [is_single(cc), channels(cc)] = bdata(['select single, sc_num from cells where cellid={S}'], cellid(cc));
 end
 
 spikeinfo.times = spktime;
