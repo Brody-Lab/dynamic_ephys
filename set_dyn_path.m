efd9332f9e6490237e6d84fb32de0b6e217c7557
tyler <jboydmeredith@gmail.com>
2020-10-01 13:17:08 -0400

    update paths

--- set_dyn_path.m
+++ set_dyn_path.m
@@ -9,28 +9,42 @@ end
 
 switch user
     case 'Tyler'
-        spikes_dir          = '~/projects/pbups_dyn/data/phys';
+        project_dir         = '~/projects/pbups_dyn/';
+        spikes_dir          = fullfile(project_dir, 'data/phys');
+        fig_dir             = fullfile(project_dir,'figures');
+        code_dir            = fullfile(project_dir, 'code');
+
     case 'Alex'
-        spikes_dir          = '~/Dropbox/spikes';
+        project_dir         = '~/Dropbox/spikes/';
+        spikes_dir          = project_dir;
+        fig_dir             = fullfile(project_dir,'figures');
+
     otherwise
         fprintf('you should pick a spikes directory')
         keyboard
 end
-spikes_fig_dir      = '~/Dropbox/spikes/figures';
-
 celldat_filename    = 'dyn_multi_db.mat';
 celldat_dir         = fullfile(spikes_dir, 'cell_packager_data');
 spikes_bin_dir      = fullfile(spikes_dir, 'bin');
+
 tuning_curves_dir   = fullfile(spikes_bin_dir, 'tuning_curves');
 ratter_dir          = '~/ratter';
 tim_code_dir        = fullfile(ratter_dir, 'Manuscripts/TimHanks/PBupsPhys/Code/');
-p.psth_fig_dir      = fullfile(spikes_fig_dir, 'PSTH');
+p.model_fits_dir    = fullfile(project_dir, 'results');
+p.model_mean_dir    =  fullfile(project_dir,'model_mean');
+p.model_dir    =  fullfile(project_dir,'model');
+p.spikes_dir        = spikes_dir;
+p.fig_dir           = fig_dir;
+p.psth_fig_dir      = fullfile(fig_dir, 'PSTH');
+p.sta_fig_dir      = fullfile(fig_dir, 'STA');
 p.celldat_filename  = celldat_filename;
 p.celldat_dir       = celldat_dir;
 p.spikes_bin_dir    = spikes_bin_dir;
 p.tuning_curves_dir = tuning_curves_dir;
 p.ratter_dir        = ratter_dir;
 p.tim_code_dir      = tim_code_dir;
+p.ephys_summary_dir = fullfile(spikes_bin_dir, 'ephys_summary');
+
 
 if pathup
     addpath(tim_code_dir, fullfile(tim_code_dir, 'Carlosbin'))
@@ -47,6 +61,12 @@ if pathup
     addpath(celldat_dir)
     addpath(spikes_bin_dir)
     addpath(tuning_curves_dir)
+    
+    addpath(genpath(code_dir));
+
 end
 
 
+if ~exist(p.psth_fig_dir)
+    mkdir(p.psth_fig_dir);
+end
