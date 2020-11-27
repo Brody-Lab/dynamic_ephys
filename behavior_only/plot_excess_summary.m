e6ade1c5a8f3a3e76d447d44276a47402e8ed1e1
tyler <jboydmeredith@gmail.com>
2020-11-27 12:30:19 -0500

    fix excess summary

--- behavior_only/plot_excess_summary.m
+++ behavior_only/plot_excess_summary.m
@@ -1,17 +1,23 @@
-cd ~/Dropbox/spikes/bin/behavior_only/
-load ~/Dropbox/dynamic/model_fits/ANALYSIS/ephys/group_analysis.mat
+clear
+dp = set_dyn_path;
+gapath = fullfile(dp.data_dir, 'group_analysis.mat');
+load(gapath)
 
 for i=1:length(F)
     close all
     if isfield(F{i}, 'clicks')
         plot_excess_rates(F{i}.clicks)
-        print(['/home/alex/Dropbox/spikes/figures/task_figure/' F{i}.rat '_excess'], '-dsvg')
+        print(fullfile(dp.fig_dir, [F{i}.rat '_excess']), '-dsvg')
     else
-        addpath ~/Dropbox/dynamic/check_rats
-        load(['~/Dropbox/dynamic/model_fits/FITS/ephys/fit_analytical_' F{i}.rat '.mat'])
+        load(fullfile(dp.model_fits_dir, ['fit_analytical_' F{i}.rat '.mat']));
+        load(fullfile(dp.data_dir, F{i}.rat))
+
         p = struct();
         clicks = compute_excess_rates_data(data,p) 
+        F{i}.clicks = clicks;
     end
 end
+
+save(gapath,'F','analysis')
 
 
