bda9a6014a47aea6c7afaff5905e4facb1a43eb3
tyler <jboydmeredith@gmail.com>
2020-11-27 12:28:31 -0500

    fix plot comparison sta

--- state_switches/plot_comparison_STA.m
+++ state_switches/plot_comparison_STA.m
@@ -3,9 +3,11 @@ if nargin < 1
     savefig = 0;
 end
 
+which_correction_str = '_bonferroni_modified_0';
+dp = set_dyn_path;
 % make fig comparing fraction of cells for each alignment
 figure(1); clf; hold on;
-load(['../figures/STA/fraction_data_model.mat'])
+load(fullfile(dp.sta_fig_dir, ['fraction_data_model' which_correction_str '.mat']))
 p = movmean(average_siggy,3);
 se = 1.96.*sqrt((p.*(1-p))./n);
 %shadedErrorBar(time_vec, p.*100, se.*100, 'k',0.1);
@@ -14,7 +16,7 @@ plot(time_vec, p.*100, 'k','linewidth',2)
 %plot(time_vec, repmat(mean(average_siggy.*100),size(time_vec)),'k--')
 
 
-load(['../figures/STA/fraction_data_generative.mat'])
+load(fullfile(dp.sta_fig_dir,['fraction_data_generative' which_correction_str '.mat']))
 p = movmean(average_siggy,3);
 se = 1.96.*sqrt((p.*(1-p))./n);
 %shadedErrorBar(time_vec, p.*100, se.*100, 'b',0.1);
@@ -39,15 +41,15 @@ fig.PaperSize = [6 6];
 legend({'Model', 'Generative'})
 legend boxoff
 if savefig
-print(fig, ['../figures/STA/fraction_comparison_switch_encoding.svg'],'-dsvg')
+print(fig, fullfile(dp.sta_fig_dir,'fraction_comparison_switch_encoding.svg'),'-dsvg')
 end
 
 figure(2); clf; hold on;
-load(['../figures/STA/switch_times_model.mat'])
+load(fullfile(dp.sta_fig_dir, ['switch_times_model' which_correction_str '.mat']))
 plot(sort(middle_ts),linspace(1,100,length(middle_ts)),'k-','linewidth',2)
 
 
-load(['../figures/STA/switch_times_generative.mat'])
+load(fullfile(dp.sta_fig_dir, ['switch_times_generative' which_correction_str '.mat']))
 plot(sort(middle_ts),linspace(1,100,length(middle_ts)),'r-','linewidth',2)
 plot([0 0], ylim, 'k--')
 ylabel('Cell # sorted by switch time')
@@ -61,7 +63,7 @@ fig.PaperSize = [4 4];
 legend({'Model', 'Generative'})
 legend boxoff
 if savefig
-print(fig, ['../figures/STA/distribution_switch_times_comparison.svg'],'-dsvg')
+print(fig, fullfile(dp.sta_fig_dir, distribution_switch_times_comparison.svg),'-dsvg')
 end
 
 
