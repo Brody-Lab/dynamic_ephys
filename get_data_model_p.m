f51741250726dfe5d8d684f0876209a7d5b089ac
tyler <jboydmeredith@gmail.com>
2020-12-22 09:38:13 -0500

    configure alternate arguments for get posterior

--- get_data_model_p.m
+++ get_data_model_p.m
@@ -1,30 +1,59 @@
-function [model, constant_x, allsame] = get_data_model_p(data, vec_data)
+function [model, constant_x, allsame] = get_data_model_p(data, which_trials)
+% function [model, constant_x, allsame] = get_data_model_p(data, which_trials)
+% returns the model posterior stuct for a given session for which_trials trials
+% can take as input data either:
+%   - filepath to model posterior
+%   - sessid for finding model posterior file
+%   - struct with fields sessid and ratname
+%   - struct containing model
+% model will be returned with only which_trials (or all trials if nargin == 1)
+% output allsame tells if accumulator value is binned the same on each trial
 
-dp = set_dyn_path;
-model_p_dir = dp.model_mean_dir;
-
-model_posterior_fn = fullfile(model_p_dir, ...
-    sprintf('model_posterior_%i.mat',data.sessid));
-
-% load model posterior for all trials
-if ~exist(model_posterior_fn,'file')
-    model = compute_model_mean(data.ratname, data.sessid);
-else
-    load(model_posterior_fn, 'model');
+if isstr(data)
+    posterior_path = data;
+elseif isnumeric(data)
+    sessid  = data;
+    ratname = bdata('select ratname from sessions where sessid={S}',sessid);
+    ratname = ratname{1};
+elseif isfield(data, 'sessid')
+    sessid = data.sessid;
+    ratname = data.ratname;
+elseif isfield(data,'posterior')
+    model = data;
 end
-% Need to filter trials from model to match data variable
-model   = model(vec_data.good);
-ntrials = length(vec_data.good);
-% Check for variable a-binning, which happens with large sensory noise to save space
-numabins = length(model(1).posterior.avals);
-constant_x = model(1).posterior.avals;
-allsame = true;
-for ti=2:ntrials
-    if length(model(ti).posterior.avals) ~= numabins
-        allsame=false;
-        if length(model(ti).posterior.avals) > numabins
-            numabins=length(model(ti).posterior.avals);
-            constant_x = -10:0.1:10;
-        end
+
+if ~exist('model','var')
+    if ~exist('posterior_path','var') 
+        dp = set_dyn_path;
+        model_p_dir = dp.model_mean_dir;
+        posterior_path = fullfile(model_p_dir, ...
+            sprintf('model_posterior_%i.mat',sessid));
     end
+    % load model posterior for all trials
+    if ~exist(posterior_path,'file')
+        if ~exist('sessid','var')
+            error('bad inputs');
+        end
+        model = compute_model_mean(ratname, sessid);
+    else
+        load(posterior_path, 'model');
+    end
+end
+
+if nargin < 2
+    which_trials = 1:length(model);
+elseif isstruct(which_trials) % if it's a struct assume it's vec_data
+    which_trials = which_trials.good;
+end
+
+% Need to filter trials from model to match data variable
+model   = model(which_trials);
+% Check for variable a-binning, which happens with large sensory noise to save space
+constant_x  = model(1).posterior.avals;
+navals      = cellfun(@(x) length(x.avals), ...
+    {model.posterior});
+allsame     = length(unique(navals)) == 1;
+
+if ~allsame
+    constant_x = -10:.1:10;
 end
