2efd47c61c31668172d5f1318ffa86fe4285c091
tyler <jboydmeredith@gmail.com>
2021-05-05 12:11:58 -0400

    speed up computation of generative state

--- compute_gen_state.m
+++ compute_gen_state.m
@@ -10,16 +10,18 @@ for i=1:length(array_data)
     ns = round((array_data(i).stim_end - array_data(i).stim_start)/1e-4);
     array_data(i).gen_state = compute_gen_state_trial(T, array_data(i).genSwitchTimes-array_data(i).stim_start, array_data(i).genEndState, 1e-4);
    
-    count = 2e-4;
-    array_data(i).gen_state_duration(1) = 1e-4;
-    for j=2:length(array_data(i).gen_state)
-        if array_data(i).gen_state(j) == array_data(i).gen_state(j-1)
-            count = count + 1e-4;
-        else
-            count = 1e-4;
-        end
-        array_data(i).gen_state_duration(j) = count;
+    
+    trial_t = 1e-4*ones(size(array_data(i).gen_state(1:end)));
+    gen_state_dur       = cumsum(trial_t);
+    gen_state_switches  = [0; diff(array_data(i).gen_state)];
+    gen_state_switch_ix = find(gen_state_switches);
+    gen_state_diff      = zeros(size(array_data(i).gen_state));
+    
+    for ss = 1:length(gen_state_switch_ix)
+        ii = gen_state_switch_ix(ss);
+        gen_state_dur(ii:end) = gen_state_dur(ii:end) - gen_state_dur(ii);
     end
+    array_data(i).gen_state_duration = gen_state_dur;
 end
 
 
