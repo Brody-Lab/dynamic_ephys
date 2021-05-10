a94c89a27aa25974da0fe6e68bb0a857fe35aa43
tyler <jboydmeredith@gmail.com>
2021-05-10 15:13:06 -0400

    use size to get n rows instead of rows function

--- color_set.m
+++ color_set.m
@@ -6,7 +6,7 @@ cmap = dyn_cmap.^0.6;
 %   cmap = rainbow_colormap;
 
    if nclasses==1,
-      g = round(0.5*rows(cmap));
+      g = round(0.5*size(cmap,1));
       C = cmap(g,:);
       return;
    end;
@@ -14,7 +14,7 @@ cmap = dyn_cmap.^0.6;
    C = zeros(nclasses, 3);
    
    for i=1:nclasses,
-      g = ((i-1)/(nclasses-1))*(rows(cmap)-1) + 1;
+      g = ((i-1)/(nclasses-1))*(size(cmap,1)-1) + 1;
       g = round(g);
       C(i,:) = cmap(g,:);
    end;
