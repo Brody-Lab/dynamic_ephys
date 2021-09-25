0717956e902b514c3a64c0154e49b923a98bd2c2
tyler <jboydmeredith@gmail.com>
2021-09-25 13:50:25 -0400

    clean up excess rates code

--- behavior_only/plot_excess_rates.m
+++ behavior_only/plot_excess_rates.m
@@ -16,11 +16,8 @@ else
 end
 ax = axes;
 smoothing = 25;
-%nice_color        = {,  };
 
 hold on;
-%plot(clicks.timepoint, clicks.RexRat, 'color', p.nice_color{1});
-%plot(clicks.timepoint, clicks.LexRat, 'color', p.nice_color{2});
 R = movmean(clicks.RexRat,smoothing);
 L = movmean(clicks.LexRat,smoothing);
 Rs= movmean(clicks.RstdRat,smoothing);
@@ -32,34 +29,20 @@ Ln = movmean(clicks.LexN,smoothing);
 Rsn= movmean(clicks.RstdN,smoothing);
 Lsn= movmean(clicks.LstdN,smoothing);
 
-
 shadedErrorBar(clicks.timepoint,Rn,Rsn,{'color',p.Results.model_color},0)  
 shadedErrorBar(clicks.timepoint,Ln,Lsn, {'color',p.Results.model_color},0)  
-%plot(clicks.timepoint,Rn,'color',p.Results.model_color,'linewidth',2)
-%hold on
-%plot(clicks.timepoint,Ln,'color',p.Results.model_color,'linewidth',2)
 end
 
-h1 = shadedErrorBar(clicks.timepoint,R,Rs, {'Color', p.Results.right_color},1)  
-h2 = shadedErrorBar(clicks.timepoint,L,Ls, {'Color', p.Results.left_color},1)  
-delete h1.mainLine;
-delete h2.mainLine;
-
-
-
-
-%plot(clicks.timepoint, clicks.fitRat.a.*exp(clicks.fitRat.b.*clicks.timepoint),'r');
+h1 = shadedErrorBar(clicks.timepoint,R,Rs, {'Color', p.Results.right_color},1);  
+h2 = shadedErrorBar(clicks.timepoint,L,Ls, {'Color', p.Results.left_color},1);  
+if isfield(h1,'mainLine')
+    rmfield(h1, 'mainLine');
+    rmfield(h2, 'mainLine');
+end
 
 xlim(clicks.xlim)
-% set(gca,'fontsize',20);
-% ylabel('relative weight','fontsize',20)
-% xlabel('time from end of trial (s)', 'fontsize',20)
 axis square
-% fig = gcf;
-% fig.PaperUnits = 'inches';
-% fig.PaperPosition = [0 0 6 6];
-% fig.PaperPositionMode = 'Manual';
-% fig.PaperSize = [6 6];
+
 ylim([-8 8])
 
 
