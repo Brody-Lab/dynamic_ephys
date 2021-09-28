da8eef309616992163f1b8be36b6146b6eefe5a7
tyler <jboydmeredith@gmail.com>
2021-09-28 18:23:45 -0400

    make make figs more legible

--- make_figures.m
+++ make_figures.m
@@ -4,18 +4,17 @@
 % Before running this code, you should
 % open set_dyn_path.m and edit the path configuration fields
 edit set_dyn_path
+
 % you also need to make sure to init and update the accumulation-model
 % submodule. you can do this by typing `git submodule init` followed by
 % `git submodule update` into the command line from the directory where
 % you've cloned this repo
 
-
-
 %% run path config
 dp = set_dyn_path;
-%% mkae fig 1 panels
+%% make fig 1 panels and S1-4 panels
 print_behavior_panels;
-%% make fig 2 panels
+%% make fig 2 panels and S5-6 panels
 close all
 clear 
 print_ephys_panels
@@ -32,16 +31,20 @@ close all
 clear 
 use_switches = 1;
 print_tuning_curve_panels;
-
+%%
+close all
+clear 
 %% extra supplementary stuff
 % fig s1-4 get made in print_behavior_panels
 % fig s5-6 get made in print_ephys_panels
-%% fig s7-9
+%% fig s7 
+random_walk_check
 
 %% fig s8
 posterior_dist_supp
-%%
-random_walk_check
+%% fig s9
+posterior_model_validation
+
 %% fig s10 rank 1 review response
 rank1_review_response
 
